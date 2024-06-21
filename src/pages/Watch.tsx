da29fc2581d06b0936653c75302967f78c48a808
Temirov Erali <eralitemirov129@gmail.com>
2024-06-21 15:36:03 +0500

    first

--- src/pages/Watch.tsx
+++ src/pages/Watch.tsx
@@ -1,11 +1,12 @@
-import Navbar from "../components/Navbar";
-
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 
 export default function Watch() {
   return (
     <div>
-        <Navbar/>
-       nomoz vaqtlari
+      <Navbar />
+      nomoz vaqtlari
+      <Footer className={''} />
     </div>
   )
 }
